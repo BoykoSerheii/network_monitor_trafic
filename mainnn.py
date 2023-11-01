f06473246e38ec5c47605ee3fe7cc02470fe6d7b
BoykoSerheii <serega-boyko1@ukr.net>
2023-11-01 21:28:57 +0200

    Додавання контуру Canvas та зміна кольорів.

--- mainnn.py
+++ mainnn.py
@@ -18,12 +18,14 @@ canvas.pack(side=LEFT, fill=Y)
 title = Label(canvas, text="Доступні мережі:", bg="#fafafa", font=40)
 title.pack()
 
-graf = Canvas(root, bg='grey', width=560, height=320)
+graf = Canvas(root, bg='#E8E8E8', width=560, height=320)
 graf.pack(anchor=NE)
 
 for i in range(0, 280, 40):
-    graf.create_line(0, i+40, 560, i+40)
+    graf.create_line(0, i + 40, 560, i + 40, fill="#C7C7C7")
     print(i)
+
+graf.create_rectangle(3, 3, 558, 318, outline="#6E6E6E", width=2)
 
 for nic, addrs in psutil.net_if_addrs().items():
     btn = ttk.Button(canvas, text="%s:" % (nic))
