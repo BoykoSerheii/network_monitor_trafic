ba0b5445c85de9f738e5bc61d77038ae058e218a
BoykoSerheii <serega-boyko1@ukr.net>
2023-11-01 20:07:39 +0200

    Редаговано функцію створення кнопок, зміна розміра поля, видалення розміру Canvas.

--- mainnn.py
+++ mainnn.py
@@ -1,25 +1,25 @@
 from tkinter import *
 from tkinter import  ttk
+import psutil
 
 root = Tk()
 max_num = [0, 0]
 
 root['bg'] = '#fafafa'
 root.title('Monitor network')
-root.geometry('600x450')
+root.geometry('800x450')
 
 root.resizable()
 #root.resizable(width=False, height=False)
 
-canvas = Canvas(root, width=600, height=450)
+canvas = Canvas(root)
 canvas.pack(side=LEFT, fill=Y)
 
 title = Label(canvas, text="Доступні мережі:", bg="#fafafa", font=40)
 title.pack()
 
-for r in range(3):
-    for c in range(3):
-        btn = ttk.Button(canvas, text=f"({r},{c})")
-        btn.pack()
+for nic, addrs in psutil.net_if_addrs().items():
+    btn = ttk.Button(canvas, text="%s:" % (nic))
+    btn.pack(fill=X)
 
 root.mainloop()
