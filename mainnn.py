1c190aea79438f14f5576d002d7901658a62e5f1
BoykoSerheii <serega-boyko1@ukr.net>
2023-11-01 20:39:22 +0200

    Додано поле для графіків.

--- mainnn.py
+++ mainnn.py
@@ -18,6 +18,9 @@ canvas.pack(side=LEFT, fill=Y)
 title = Label(canvas, text="Доступні мережі:", bg="#fafafa", font=40)
 title.pack()
 
+graf = Canvas(root, bg='red', width=560, height=300)
+graf.pack(anchor=NE)
+
 for nic, addrs in psutil.net_if_addrs().items():
     btn = ttk.Button(canvas, text="%s:" % (nic))
     btn.pack(fill=X, ipadx=10, ipady=10)
